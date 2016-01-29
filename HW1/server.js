1b1a35698f721d93e7e95305e457026648421852
Adam White <white2a3@mail.uc.edu>
2016-01-29 02:03:47 +0000

    Use iptables to forward port 80 to port 8080.

--- HW1/server.js
+++ HW1/server.js
@@ -6,6 +6,6 @@ app.configure(function() {
   app.use(express.static(__dirname + '/public'));
 });
 
-var port = 80;
+var port = 8080;
 app.listen(port);
 console.log('Express server started on port %s', port);
