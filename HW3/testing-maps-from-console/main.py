39a0ccaf951f7f1d4c6b7cae0a6b28cfb4343b88
Adam White <adam.nathaniel.white@gmail.com>
2016-02-26 19:40:20 -0500

    working - geocoding of typed location to coordinates. Display marker on static map.

--- HW3/testing-maps-from-console/main.py
+++ HW3/testing-maps-from-console/main.py
@@ -1,11 +1,15 @@
 import cgi
 import datetime
 import webapp2
+import urllib2
+import json
 
 from google.appengine.ext import ndb
 from google.appengine.api import users
 
 guestbook_key = ndb.Key('Guestbook', 'default_guestbook')
+
+GMAPS_API_KEY = "AIzaSyB16N6RHY71J_sZlGupTntG4vvx1rF_rGc"
 
 class Greeting(ndb.Model):
   nickname = ndb.UserProperty()
@@ -27,6 +31,7 @@ class MainPage(webapp2.RequestHandler):
                     guestbook_key)
 
     interactionsArray = []
+    coords = []
     						
     for interaction in interactions:
       if interaction.nickname != None:
@@ -41,16 +46,56 @@ class MainPage(webapp2.RequestHandler):
 ##        self.response.out.write('An anonymous person wrote:')
 ##      self.response.out.write('<blockquote>%s</blockquote>' %
 ##                              cgi.escape(interaction.location))
-
-    GMAPS_API_KEY = "AIzaSyB16N6RHY71J_sZlGupTntG4vvx1rF_rGc"
+        
+    ## GEOCODING
+    stringSearchLocation = interactionsArray[0].location.replace(" ", "+")
+    geocodeRequestURL = """https://maps.googleapis.com/maps/api/geocode/json?address=""" + stringSearchLocation + """+Chipotle&key=""" + GMAPS_API_KEY
+    
+    geocodeRequest = urllib2.urlopen(geocodeRequestURL)
+    data = json.load(geocodeRequest)
+    #self.response.out.write("<br />DATA FROM REQUEST: " + geocodeRequestURL + " <br />")
+    #self.response.out.write(data)
+    #self.response.out.write("<br />")
+    newInputCoord = []
+    newInputCoord.append( data['results'][0]['geometry']['location']['lat'] )
+    newInputCoord.append( data['results'][0]['geometry']['location']['lng'] )
+    #self.response.out.write(newInputCoord)
+    
+    ## MAP IFRAME
     self.response.out.write("""
-      <iframe
-        width="600"
-        height="450"
-        frameborder="0" style="border:0"
-        src="https://www.google.com/maps/embed/v1/place?key=""" + GMAPS_API_KEY + """
-          &q=""" + interactionsArray[0].location + """+Chipotle" allowfullscreen>
-      </iframe><br />""")
+      <img width="600" src="http://maps.googleapis.com/maps/api/staticmap?center=united+states&zoom=3&scale=false&size=600x300&maptype=roadmap&key=AIzaSyB16N6RHY71J_sZlGupTntG4vvx1rF_rGc&format=png&visual_refresh=true
+      &markers=size:large%7color:0xff0000%7c""" + str(newInputCoord[0]) + """,+""" + str(newInputCoord[1]) + """
+      ">""")
+##    self.response.out.write("""
+##      <iframe
+##        width="600"
+##        height="450"
+##        frameborder="0" style="border:0"
+##        src="https://maps.google.com/maps/api/staticmap?center=Brooklyn+Bridge,New+York,NY&zoom=14&size=512x512&maptype=roadmap&markers=color:blue|label:S|40.702147,-74.015794&markers=color:green|label:G|40.711614,-74.012318&markers=color:red|color:red|label:C|40.718217,-73.998284&sensor=false&key=AIzaSyB16N6RHY71J_sZlGupTntG4vvx1rF_rGc" allowfullscreen>
+##      </iframe><br />""")
+##    self.response.out.write("""
+##      <iframe
+##        width="600"
+##        height="450"
+##        frameborder="0" style="border:0"
+##        src="https://maps.google.com/maps/api/staticmap?center=Brooklyn+Bridge,New+York,NY&zoom=14&size=512x512&maptype=roadmap&markers=color:blue|label:S|40.702147,-74.015794&markers=color:green|label:G|40.711614,-74.012318&markers=color:red|color:red|label:C|40.718217,-73.998284&sensor=false&key=AIzaSyALo9jvP0xkfCm_w0Muaw1H4tBIJXgnucc" allowfullscreen>
+##      </iframe><br />""")
+##    self.response.out.write("""
+##      <iframe
+##        width="600"
+##        height="450"
+##        frameborder="0" style="border:0"
+##        src="https://www.google.com/maps/embed/v1/place?key=""" + GMAPS_API_KEY + """
+##          &q=""" + str(newInputCoord[0]) + "," + str(newInputCoord[1]) + """+Chipotle" allowfullscreen>
+##      </iframe><br />""")
+##    self.response.out.write("""
+##      <iframe
+##        width="600"
+##        height="450"
+##        frameborder="0" style="border:0"
+##        src="https://www.google.com/maps/embed/v1/place?key=""" + GMAPS_API_KEY + """
+##          &q=""" + interactionsArray[0].location + """+Chipotle" allowfullscreen>
+##      </iframe><br />""")
 
 
     self.response.out.write("""
