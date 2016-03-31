babfcae2164afb9c2e4bae6a31df4f9b56aef88f
Adam White <adam.nathaniel.white@gmail.com>
2016-03-30 22:47:14 -0400

    generate primes, not using map/reduce

--- HW4-mincemeat/primes.py
+++ HW4-mincemeat/primes.py
@@ -4,36 +4,104 @@ file = open('small.txt', 'r')
 data = list(file)
 file.close()
 
-datasource = dict((i, i+1) for i in range(2, 100))
-#datasource = dict((i, i+1) for i in range(2000000, 10000000))
-#datasource = dict(enumerate(data))
+array = range(3, 201, 2)
+bins = [[0],[1],[2],[3],[4]]
+for i in array:
+    bins[i%5].append(i)
+datasource = dict( (k[0], k[1:]) for k in bins)
+#print datasource
 
-def mapfn(k, v):
-    reducerNum = v % 4
-    yield v, v
+##v = [0,1,2,3,4,5,6,7,8,9,10,11,12]
+##for thing in v:
+##        #if palindrome
+##        if (str(thing) == str(thing)[::-1]):
+##            print "palindrome!", thing
 
-def reducefn(k, x):
-    print "x is ", x
-    #Is it a prime?
-    if x >= 2:
-##        print "y is", y
-##        return None
-##        for y in range(2, x):
-##            print "y is", y
-##            if not ( x % y ):
-##                return None
-    else:
-	return None
-    
-    #Is it a palindrome
-    
-    return x
+#v=[3,5,7,9,11]
+v = array
+##a = []
+##for val in v:
+##    print val
+##    tupl = (val, True)
+##    a.append(tupl)
+##print a
+###a = [True] * limit
+###for (i, isprime) in a:
+##for (i, thing) in enumerate(a):
+##    (v, isprime) = thing
+##    print i, isprime
+##    if isprime:
+##        #yield i
+##        for n in xrange(i*i, v[len(v)-1], i):
+##            print "n=", n
+##            a[n] = False
 
-s = mincemeat.Server()
-s.datasource = datasource
-s.mapfn = mapfn
-s.reducefn = reducefn
 
-results = s.run_server(password="changeme")
-print results
-print "done"
+limit = v[len(v)-1]
+print "limit is", limit
+a = [True] * limit
+a[0] = a[1] = False
+
+for (i, isprime) in enumerate(a):
+    if isprime:
+        print "yield i", i
+        for n in xrange(i*i, limit, i):     # Mark factors non-prime
+            a[n] = False
+for (i, isprime) in enumerate(a):
+    if isprime:
+        print "We have a prime", i
+
+##def mapfn(k, v):
+##    a = [True] * limit
+##    a[0] = a[1] = False
+##
+##    for (i, isprime) in enumerate(a):
+##        if isprime:
+##            yield i
+##            for n in xrange(i*i, limit, i):
+##                a[n] = False
+####    for thing in v:
+####        #if palindrome
+####        if (str(thing) == str(thing)[::-1]):
+####            print "palindrome!", thing
+####
+####            #if prime
+####            
+####
+####            yield thing, thing
+##
+##
+##def reducefn(k, x):
+##    return x
+##
+##
+##
+####def mapfn(k, v):
+####    reducerNum = v % 4
+####    yield v, v
+####
+####def reducefn(k, x):
+######    print "x is ", x
+######    #Is it a prime?
+######    if x >= 2:
+########        print "y is", y
+########        return None
+########        for y in range(2, x):
+########            print "y is", y
+########            if not ( x % y ):
+########                return None
+######    else:
+######	return None
+######    
+######    #Is it a palindrome
+####    
+####    return x
+##
+##s = mincemeat.Server()
+##s.datasource = datasource
+##s.mapfn = mapfn
+##s.reducefn = reducefn
+##
+##results = s.run_server(password="changeme")
+##print results
+##print "done"
