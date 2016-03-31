62b72b77ccf00adac1433dbe78e7485cf8a3ebbb
Adam White <adam.nathaniel.white@gmail.com>
2016-03-31 16:16:46 -0400

    Reconfigure output

--- HW4-mincemeat/passCrack.py
+++ HW4-mincemeat/passCrack.py
@@ -7,21 +7,25 @@ start_time = timeit.default_timer()
 #Assume passwords are 4 or fewer characters containing only lowercase letters and numbers.
 possibleChars = "abcdefghijklmnopqrstuvwxyz0123456789"
 passwordSpace = []
+passwordSpace3 = []
 for char1 in list(possibleChars):
     passwordSpace.append(char1)
     for char2 in list(possibleChars):
         passwordSpace.append(char1 + char2)
         for char3 in list(possibleChars):
             passwordSpace.append(char1 + char2 + char3)
+            #passwordSpace3.append(char1 + char2 + char3)
             for char4 in list(possibleChars):
                 passwordSpace.append(char1 + char2 + char3 + char4)
-print "created passwordSpace"
 
 #user input
 userInputHash = sys.argv[1]
 passwordInputDoubleSpace = []
 for i in range(0, len(passwordSpace)-1):
     passwordInputDoubleSpace.append([userInputHash, passwordSpace[i]])
+##binsDict = {}
+##for firstThreeChars in passwordSpace3:
+##    binsDict[firstThreeChars] = [
 
 #datasource = dict(enumerate(passwordSpace))
 datasource = dict(enumerate(passwordInputDoubleSpace))
@@ -36,7 +40,7 @@ def mapfn(k, v):
     #if five == "d077f" or five == "0832c" or five == "1a1dc" or five == "ee269" or five == "0fe63":
     if vHashed[:5] == v[0]:
         print v[1], "-->", vHashed[:5]
-        yield v[1], vHashed[:5]
+        yield vHashed[:5], v[1]
     #else:
         #print v, "-//->", vHashed[:5]
 
